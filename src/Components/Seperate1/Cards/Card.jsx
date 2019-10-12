903299467d817bf43f123a0630303d45e84fac4c
Burak <brkcrdk@gmail.com>
2019-10-12 22:13:31 +0300

    Card Style

---
+++ src/Components/Seperate1/Cards/Card.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <div className="card">
+      <img src="..." class="card-img-top" alt="..." />
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">Some</p>
+        <a href="#?" class="btn btn-primary">
+          Go
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
--- src/Components/Seperate1/Cards/Cards.jsx
+++ src/Components/Seperate1/Cards/Cards.jsx
@@ -1,13 +1,54 @@
 import React from "react";
-
+import "./Cards.css";
+import Card from "./Card";
 const Cards = () => {
   return (
-    <div className="row text-center">
-      <div className="col-md-3">Cart 1</div>
-      <div className="col-md-3">Cart 1</div>
-      <div className="col-md-3">Cart 1</div>
-      <div className="col-md-3">Cart 1</div>
-    </div>
+    <>
+      <div id="mobileCards" className="container-fluid">
+        <div class="accordion" id="accordionExample">
+          <div class="card" style={{ borderRadius: "1em" }}>
+            <div class="card-header" id="headingOne">
+              <h2 class="mb-0">
+                <button
+                  class="btn"
+                  type="button"
+                  data-toggle="collapse"
+                  data-target="#collapseOne"
+                  aria-expanded="true"
+                  aria-controls="collapseOne">
+                  Collapsible Group Item #1
+                </button>
+              </h2>
+            </div>
+            <div
+              id="collapseOne"
+              class="collapse show"
+              aria-labelledby="headingOne"
+              data-parent="#accordionExample">
+              <div class="card-body">
+                <Card />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div id="desktopCards">
+        <div className="row text-center">
+          <div className="col-md-3">
+            <Card />
+          </div>
+          <div className="col-md-3">
+            <Card />
+          </div>
+          <div className="col-md-3">
+            <Card />
+          </div>
+          <div className="col-md-3">
+            <Card />
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
