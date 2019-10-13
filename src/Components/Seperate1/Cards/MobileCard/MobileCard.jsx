c513a75bac079a6bae66509a631b27403dc9f4d8
Burak <brkcrdk@gmail.com>
2019-10-13 15:00:37 +0300

    Mobile card designed

--- src/Components/Seperate1/Cards/MobileCard/MobileCard.jsx
+++ src/Components/Seperate1/Cards/MobileCard/MobileCard.jsx
@@ -2,38 +2,8 @@ import React from "react";
 import "./MobileCard.css";
 const MobileCard = (props) => {
   return (
-    <div className="card container-fluid" id="mobile-card">
-      <div className="row">
-        <div className="col-xs-6">
-          <img
-            src="https://picsum.photos/200/300"
-            class="card-img-top"
-            alt="..."
-          />
-        </div>
-        <div className="col-xs-4">
-          <div className="card-body">
-            <div id="badge-title">
-              <span className="badge badge-dark">
-                Yakında <i className="far fa-calendar-alt"></i>
-              </span>
-              <h6>Title</h6>
-            </div>
-            <div className="row" id="card-bottom">
-              <div id="bonus">
-                <span>Bonus</span>
-                <span>-%15</span>
-              </div>
-              <div id="prices">
-                <span id="text">100</span>
-                <span>85</span>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      {/* <img src="https://picsum.photos/200/300" class="card-img-top" alt="..." />
+    <div className="card" id="mobile-card">
+      <img src="https://picsum.photos/200/300" class="card-img-top" alt="..." />
       <div className="card-body">
         <div id="badge-title">
           <span className="badge badge-dark">
@@ -46,14 +16,14 @@ const MobileCard = (props) => {
             <span>Bonus</span>
           </div>
           <div id="prices">
-            <span>-%15</span>
+            <span id="push-right">-%15</span>
             <div id="net-price">
               <span>100</span>
               <span>85</span>
             </div>
           </div>
         </div>
-      </div> */}
+      </div>
     </div>
   );
 };
