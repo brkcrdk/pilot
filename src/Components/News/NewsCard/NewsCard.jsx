87b1ab2cd7261b7d16a0358b64d7cc89490eedcd
Burak <brkcrdk@gmail.com>
2019-10-13 21:05:40 +0300

    Card Style adjustments

--- src/Components/News/NewsCard/NewsCard.jsx
+++ src/Components/News/NewsCard/NewsCard.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import "./NewsCard.css";
 import { slides } from "../../Content/Content";
 import Card from "../../Cards/Card/Card";
 const NewsCard = () => {
   return (
-    <div className="container-fluid">
+    <div className="container-fluid" id="news-container">
       <div className="row text-center">
         <div className="row">
           <div className="col-md-12">
