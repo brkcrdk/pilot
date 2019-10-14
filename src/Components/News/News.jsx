e09a0984206333720892cf0e778b97c2ccc9509b
Burak <brkcrdk@gmail.com>
2019-10-14 18:04:15 +0300

    data fetched and object remapped

--- src/Components/News/News.jsx
+++ src/Components/News/News.jsx
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./News.css";
+import axios from "axios";
 import NewsCard from "./NewsCard/NewsCard";
 import MobileNews from "./MobileNews/MobileNews";
 const News = () => {
+  const [data, setData] = useState();
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users/")
+      .then(({ data }) => {
+        setData(data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+  console.log(data);
+  //  const newArray = subSlides.map((slide, key) => {
+  //    return (slide = { ...slide, name: "Burak", price: "12" });
+  //  });
+  //  console.log(newArray);
   return (
     <div id="news-container" className="container">
       <div id="splitter">
