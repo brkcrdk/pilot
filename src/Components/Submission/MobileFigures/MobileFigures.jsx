45616e6d3cb94b32346e3b3144fc3c3fa85f2db8
Burak <brkcrdk@gmail.com>
2019-10-14 13:33:00 +0300

    Carousels added for both mobile and desktop

--- src/Components/Submission/MobileFigures/MobileFigures.jsx
+++ src/Components/Submission/MobileFigures/MobileFigures.jsx
@@ -1,17 +1,54 @@
 import React from "react";
 import "./MobileFigures.css";
 import Figure from "../Figure/Figure";
+import { subSlides } from "../../Content/Content";
 const MobileFigures = () => {
   return (
-    <div id="mobile-figure">
-      <div className="container-fluid text-center">
-        <div className="row">
-          <div className="col-sm-12">
-            <Figure />
+    <div id="mobile-figure" className="carousel slide" data-ride="carousel">
+      <ol className="carousel-indicators">
+        {subSlides.map((slide, index) => (
+          <li
+            className={`${index === 0 ? "active" : ""}`}
+            data-target="#mobile-figure"
+            data-slide-to={`${index}`}
+          />
+        ))}
+      </ol>
+      <div className="carousel-inner">
+        {subSlides.map((slide, index) => (
+          <div className={`carousel-item ${index === 0 ? "active" : ""}`}>
+            <div className="row">
+              <div className="col-sm-12">
+                <Figure />
+              </div>
+            </div>
           </div>
-        </div>
+        ))}
       </div>
+      <a
+        className="carousel-control-prev"
+        href="#mobile-figure"
+        role="button"
+        data-slide="prev">
+        <span className="sr-only">Previous</span>
+      </a>
+      <a
+        className="carousel-control-next"
+        href="#mobile-figure"
+        role="button"
+        data-slide="next">
+        <span className="sr-only">Next</span>
+      </a>
     </div>
+    // <div id="mobile-figure">
+    //   <div className="container-fluid text-center">
+    //     <div className="row">
+    //       <div className="col-sm-12">
+    //         <Figure />
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
   );
 };
 
