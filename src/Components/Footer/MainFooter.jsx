1b51b114db2adc4e8e32658bd2f85d435aa6522e
Burak <brkcrdk@gmail.com>
2019-10-14 15:18:30 +0300

    Footer design

--- src/Components/Footer/MainFooter.jsx
+++ src/Components/Footer/MainFooter.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const MainFooter = () => {
   return (
-    <div className="container">
+    <div className="container text-center">
       <div className="row">
         <div className="col-md">
           <div id="footer-brand">
@@ -12,10 +12,45 @@ const MainFooter = () => {
             </span>
           </div>
         </div>
-        <div className="col-md">2</div>
-        <div className="col-md">2</div>
-        <div className="col-md">3</div>
-        <div className="col-md">3</div>
+        <div className="col-md">
+          <span className="titles">About Us</span>
+          <ul>
+            <li>Lorem ipsum dolor</li>
+            <li>Sit amet consectetuer</li>
+            <li>Adipiscing elit</li>
+            <li>Sed diam nonummy nibh</li>
+            <li>Euismod tincidunt</li>
+            <li>Laoreet dolore magna</li>
+            <li>Aliquam</li>
+            <li>Erat</li>
+          </ul>
+        </div>
+        <div className="col-md">
+          <span className="titles">Customer</span>
+          <ul>
+            <li>Volutpat</li>
+            <li>Ut wisi enim</li>
+            <li>Ad minim veniam</li>
+            <li>Quis nostrud</li>
+          </ul>
+        </div>
+        <div className="col-md">
+          <span className="titles">Extras</span>
+          <ul>
+            <li>Exerci tation</li>
+            <li>Ullamcorper</li>
+            <li>Suscipit lobortis</li>
+          </ul>
+        </div>
+        <div className="col-md">
+          <span className="titles">Wallet</span>
+          <ul>
+            <li>Duis autem</li>
+            <li>Vel eum iriure</li>
+            <li>Dolor in hendrerit</li>
+            <li>In vulputate</li>
+          </ul>
+        </div>
       </div>
     </div>
   );
